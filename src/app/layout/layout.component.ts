8fb8b011140c46b03b6745ee214e1fe5f34f1300
USERCHI-0Q97M0O\Administrator <jifeng.ding@emcoo.com>
2017-12-10 01:16:52 +0800

    bar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
+  @ViewChild('aside') aside: ElementRef;
 
-  constructor() { }
+  @HostListener('window:resize')
+  onResize() {
+    this.toggle(window.innerWidth >= 992);
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  // 左侧导航显示/隐藏
+  toggle(condition: boolean) {
+    const el = this.aside.nativeElement;
+    if (condition) {
+      el.className = 'aside-navbar bg-secondary w-240';
+    } else {
+      el.className = 'aside-navbar bg-secondary w-0';
+    }
   }
 
 }
